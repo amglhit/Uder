a902e3fa175c8d6a1d36b66aa7c84aa64ff683c2
amu <amuguleng@mobike.com>
2018-09-07 10:52:38 +0800

    init

---
+++ feature/src/main/java/com/amglhit/uder/feature/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.amglhit.uder.feature
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_main)
+  }
+}
